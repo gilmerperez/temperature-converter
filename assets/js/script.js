09ee26aee03465db372fcd36403db72f7f2c5b0d
gilmerperez <gilmer2002@outlook.com>
2025-05-07 15:17:45 -0400

    Fixed mobile responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,16 @@ let temperature;
 function convert(event) {
   // Prevent the page from refreshing
   event.preventDefault();
-  // If user wants to convert from celsius to fahrenheit
-  if (toFahrenheit.checked) {
-    temperature = Number(textBox.value);
-    temperature = (temperature * 9) / 5 + 32;
-    result.textContent = temperature.toFixed(1) + " °Fahrenheit";
-    // If user wants to convert from fahrenheit to celsius
-  } else if (toCelsius.checked) {
+  // If user wants to convert from fahrenheit to celsius
+  if (toCelsius.checked) {
     temperature = Number(textBox.value);
     temperature = (temperature - 32) * (5 / 9);
     result.textContent = temperature.toFixed(1) + " °Celsius";
+    // If user wants to convert from celsius to fahrenheit
+  } else if (toFahrenheit.checked) {
+    temperature = Number(textBox.value);
+    temperature = (temperature * 9) / 5 + 32;
+    result.textContent = temperature.toFixed(1) + " °Fahrenheit";
     // If user has not selected a conversion type
   } else {
     result.textContent = "SELECT A UNIT!";
